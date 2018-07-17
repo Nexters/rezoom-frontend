250728fca2d0e2ac8fae55021d5748bbff105320
jwkim88 <jwkim88@1thefull.com>
2018-07-18 01:13:23 +0900

    Auth.saga.js 코멘트 작성

--- src/store/Auth/Auth.saga.js
+++ src/store/Auth/Auth.saga.js
@@ -23,6 +23,11 @@ export function* login() {
      * 
      * 원래 사가로 api를 들고와야하는데 그 부분은 수정못함.. 
      * 
+     * redux-saga
+     *  1. export default function* () 함수에서 yield fork로 init시에 이벤트를 감시하고있는다
+     *  2. watchLoginRequest() 의 while이 true로 무한 루프를 돌며 yield take(LOGIN) 감시
+     *  3. store의 action이 실행되면 yield take(LOGIN) 을 실행하고 yield call(login) 실행
+     *  4. 현재 login() 함수 실행
      */
     // Api.get('http://172.30.1.26:8080/login/github');
   } catch (e) {
